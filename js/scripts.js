97b9c1957a6995c717cc988457abccaf0bf7743f
Declan Hunter <declanhunter156@icloud.com>
2021-03-13 16:02:46 -0500

    added measurement and unit to replace length dimension

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
+const measurement = document.querySelector('#select-measurement');
+const backWallDistance = document.querySelector('.listening-position-distance');
+const sideWallDistance = document.querySelector('.right-speaker-sidewall-distance');
+const leftWallDistance = document.querySelector('.left-speaker-sidewall-distance');
 const frontWallDistance = document.querySelector('.speaker-frontwall-distance');
+const displayWidth = document.querySelector('.rear-wall-dimension');
+const displayLength = document.querySelector('.left-wall-dimension');
 const submitButton = document.querySelector('#submit-button');
 const length = document.getElementById('room-length');
 const width = document.getElementById('room-width');
 
 
 submitButton.addEventListener("click", function(){
-  frontWallDistance.innerText = length.value;
-  alert(length.value);
+  displayLength.innerText = length.value + ' ' + measurement.value;
   event.preventDefault();
 });
