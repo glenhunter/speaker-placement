48179e640cbed8688562a52f79df199d88d3d317
Glen Hunter <ghunter@sonatype.com>
2021-03-13 16:01:01 -0500

    Fiddling with CSS variables & JS

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,15 @@ const frontWallDistance = document.querySelector('.speaker-frontwall-distance');
 const submitButton = document.querySelector('#submit-button');
 const length = document.getElementById('room-length');
 const width = document.getElementById('room-width');
-
+var CSSVariables = document.querySelector(':root');
 
 submitButton.addEventListener("click", function(){
+  // Set HTML text and values
   frontWallDistance.innerText = length.value;
-  alert(length.value);
+
+  // Set CSS variables
+  CSSVariables.style.setProperty('--room-length', length.value + 'px');
+
+  // Don't refreh the page on Submit
   event.preventDefault();
 });
